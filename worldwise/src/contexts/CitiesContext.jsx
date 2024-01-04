c47fdba7421b503597d3b9fa2e15ccda76899ae3
Christof Kälin <christof.kaelin@pm.me>
2024-01-04 13:37:56 +0100

    fakeauth context & login

--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -149,7 +149,7 @@ function CitiesProvider({ children }) {
 function useCities() {
   const context = useContext(CitiesContext)
   if (context === undefined)
-    throw new Error("CitiesContex was used outside the CitiesProvider")
+    throw new Error("CitiesContext was used outside the CitiesProvider")
   return context
 }
 
--- worldwise/src/pages/Login.jsx
+++ worldwise/src/pages/Login.jsx
@@ -1,16 +1,29 @@
+import { useNavigate } from "react-router-dom"
 import PageNav from "../components/PageNav"
 import styles from "./Login.module.css"
 import { useState } from "react"
+import { useAuth } from "../contexts/FakeAuthContext"
+import Button from "../components/Button"
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com")
   const [password, setPassword] = useState("qwerty")
+  const navigate = useNavigate()
+  const { isAuthenticated, login } = useAuth()
+
+  if (isAuthenticated === true) {
+    navigate("/app")
+  }
+
+  function handleSubmit() {
+    if (email && password) login(email, password)
+  }
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +45,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
