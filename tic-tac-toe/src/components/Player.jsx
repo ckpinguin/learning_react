37061a4efe432cd04c6b6aa1584b07bc80c972c8
Christof Kälin <christof.kaelin@pm.me>
2024-01-13 20:42:06 +0100

    gameboard added

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,21 +1,32 @@
 import {useState} from "react"
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
   const [isEditing, setIsEditing] = useState(false)
+  const [playerName, setPlayerName] = useState(initialName)
 
   function handleEditClick() {
     // Never forget to use this construct instead of
+    // (if updating state based on previous state)
     // directly setting the value (state updates are scheduled!)
     setIsEditing((isEditing) => !isEditing)
+  }
+
+  function handleChange(e) {
+    const name = e.target.value
+    setPlayerName(name)
   }
 
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input name="player-name" value={name}></input>
+          <input
+            name="player-name"
+            onChange={handleChange}
+            value={playerName}
+            required></input>
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
