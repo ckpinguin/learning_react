4c41cc6a5acefc098324dd2edda2fadcf09e1751
Christof Kälin <christof.kaelin@pm.me>
2024-01-17 16:17:46 +0100

    gameboard-button disabled if clicked once

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -19,10 +19,12 @@ export default function GameBoard({ onSelectSquare, turns }) {
       {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((col, colIndex) => (
+            {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
-                  {col}
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}>
+                  {playerSymbol}
                 </button>
               </li>
             ))}
