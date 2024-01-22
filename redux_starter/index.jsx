b66dd55c6d0c2d729ac5b9966d892eefb96925b5
Christof Kälin <christof.kaelin@pm.me>
2024-01-22 21:07:43 +0100

    Refactor index.jsx and package.json, add store.js with initial state and reducer

--- redux_starter/index.jsx
+++ redux_starter/index.jsx
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client"
+import { StrictMode } from "react"
+import App from "./App.jsx"
+import "./index.css"
 
-import App from './App.jsx';
-import './index.css';
+import "./src/store"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
