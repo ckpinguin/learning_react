9802ba675e7e618effb739698fe715ce26ab0377
Christof Kälin <christof.kaelin@pm.me>
2024-01-22 14:40:29 +0100

    app debug finished and started section refs & portals

--- debug_app/src/components/Results.jsx
+++ debug_app/src/components/Results.jsx
@@ -1,13 +1,15 @@
-import { calculateInvestmentResults, formatter } from '../util/investment.js';
-
+import { calculateInvestmentResults, formatter } from "../util/investment.js"
 
 export default function Results({ input }) {
-  const results = [];
-  calculateInvestmentResults(input, results);
+  const results = []
+  calculateInvestmentResults(input, results)
+  if (results.length === 0) {
+    return <p class="center">Invalid input data provided.</p>
+  }
   const initialInvestment =
     results[0].valueEndOfYear -
     results[0].interest -
-    results[0].annualInvestment;
+    results[0].annualInvestment
 
   return (
     <table id="result">
@@ -25,8 +27,8 @@ export default function Results({ input }) {
           const totalInterest =
             yearData.valueEndOfYear -
             yearData.annualInvestment * yearData.year -
-            initialInvestment;
-          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+            initialInvestment
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest
 
           return (
             <tr key={yearData.year}>
@@ -36,9 +38,9 @@ export default function Results({ input }) {
               <td>{formatter.format(totalInterest)}</td>
               <td>{formatter.format(totalAmountInvested)}</td>
             </tr>
-          );
+          )
         })}
       </tbody>
     </table>
-  );
+  )
 }
