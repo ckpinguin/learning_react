ec747a2a5682ba7efd165b6b05e9f47d72b5cbb8
Christof Kälin <christof.kaelin@pm.me>
2024-02-05 18:13:21 +0100

    refs & modals section finished

--- refs_and_portals/src/components/Player.jsx
+++ refs_and_portals/src/components/Player.jsx
@@ -15,7 +15,9 @@ export default function Player() {
       <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
         <input ref={playerName} type="text" />
-        <button onClick={handleClick}>Set Name</button>
+        <button type="button" onClick={handleClick}>
+          Set Name
+        </button>
       </p>
     </section>
   )
--- refs_and_portals/src/components/ResultModal.jsx
+++ refs_and_portals/src/components/ResultModal.jsx
@@ -1,22 +1,39 @@
-import { forwardRef } from "react"
+import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom"
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
-  return (
-    <dialog ref={ref} className="result-modal" open>
-      <h2>You {result}</h2>
+  const dialog = useRef()
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+  const userLost = remainingTime <= 0
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100)
+  // A facade or adapter to decouple the <dialog> from  outer
+  // (i.e. „calling“) components
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal()
+      },
+    }
+  })
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost ? <h2>You lost</h2> : null}
+      {!userLost ? <h2>Your score: {score}</h2> : null}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
-        <button>Close</button>
+      <form method="dialog" onSubmit={onReset}>
+        <button type="submit">Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   )
 })
 
