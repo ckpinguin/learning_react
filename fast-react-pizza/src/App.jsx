4d220420ac0fb667fc1f601cd68d133a54ba0b91
Christof Kälin <christof.kaelin@pm.me>
2024-02-14 18:47:11 +0100

    starting loader function for ReactRouter

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import Home from "./ui/Home"
+import Menu, { loader as menuLoader } from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import CreateOrder from "./features/order/CreateOrder"
+import Order from "./features/order/Order"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+    loader: menuLoader,
+  },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+])
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <h1>Hello vite!</h1>
-      </div>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
