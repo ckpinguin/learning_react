825bc4b06e1c5955c4c0deabd12a422878114c3f
Christof Kälin <christof.kaelin@protonmail.com>
2024-02-20 12:46:59 +0100

    Refactor CSS classes and import paths

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom"
 
 export default function Button({ children, disabled, to, type }) {
   const base =
-    "rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+    "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-sm text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
   const styles = {
-    primary: base + "px-4 py-3 md:px-6 md:py-4",
+    primary: base + " " + "px-4 py-3 md:px-6 md:py-4",
     secondary:
-      base +
-      "bg-stone-400 hover:bg-stone-300 focus:bg-stone-300 text-yellow-400",
-    small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+      /* It's better to redefine it completely than to use the base variable as it's nearly 
+    impossible to keep track of overwritten values etc. */
+      "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-sm text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
+    small: base + " " + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
   }
 
   if (to)
