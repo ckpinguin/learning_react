001d791f76c13af1379a80fd00b3401db5b3ee08
Christof Kälin <christof.kaelin@protonmail.com>
2024-02-20 18:50:27 +0100

    Add quantity display and update functionality to cart items

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,8 +1,9 @@
 import { Form, redirect, useNavigation, useActionData } from "react-router-dom"
-import { useState } from "react"
 import { createOrder } from "../../services/apiRestaurant"
 import Button from "../../ui/Button"
 import { useSelector } from "react-redux"
+import { getCart } from "../cart/cartSlice"
+import EmptyCart from "../cart/EmptyCart"
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,39 +11,14 @@ const isValidPhone = (str) =>
     str
   )
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-]
-
 function CreateOrder() {
   const username = useSelector((state) => state.user.username)
   const navigation = useNavigation()
   const isSubmitting = navigation.state === "submitting"
   const formErrors = useActionData()
 
-  const [withPriority, setWithPriority] = useState(false)
-  const cart = fakeCart
-
+  const cart = useSelector(getCart)
+  if (!cart.length) return <EmptyCart />
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">
@@ -93,8 +69,6 @@ function CreateOrder() {
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             name="priority"
             id="priority"
-            value={withPriority}
-            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label className="font-medium" htmlFor="priority">
             Want to yo give your order priority?
@@ -102,6 +76,8 @@ function CreateOrder() {
         </div>
 
         <div>
+          {/* We add the cart as a hidden input because we are not able to get it
+          from redux in the action below */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button type="primary" disabled={isSubmitting}>
             {isSubmitting ? "Placing order..." : "Order now"}
