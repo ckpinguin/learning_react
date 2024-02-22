635cd725e77370afff672449c8da0731896ced04
Christof Kälin <christof.kaelin@pm.me>
2024-02-22 13:55:43 +0100

    styled-components entry

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from "react"
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+import Button from "./ui/Button"
+import Input from "./ui/Input"
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello World</h1>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button type="button" onClick={() => alert("Welcome!")}>
+          Check in
+        </Button>
+        <Button type="button" onClick={() => alert("Good bye!")}>
+          Check out
+        </Button>
+        <Input type="number" name="nrguests" placeholder="Number of guests" />
+      </StyledApp>
     </>
   )
 }
