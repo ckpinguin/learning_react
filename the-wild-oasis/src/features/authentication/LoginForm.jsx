82e68f58375c5318f278d46240753353c5656c6f
Christof Kälin <christof.kaelin@protonmail.com>
2024-03-14 17:54:40 +0100

    authorization (login & logout)

--- the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin"
 import SpinnerMini from "../../ui/SpinnerMini"
 
 function LoginForm() {
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const { login, isLoading } = useLogin()
 
   function handleSubmit(e) {
@@ -16,7 +16,15 @@ function LoginForm() {
     if (!email || !password) {
       return
     }
-    login({ email, password })
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("")
+          setPassword("")
+        },
+      }
+    )
   }
 
   return (
