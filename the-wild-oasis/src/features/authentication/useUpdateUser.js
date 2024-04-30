122813f9c6f45d73df56f81b7ddc776e25de4f5c
Christof Kälin <christof.kaelin@protonmail.com>
2024-04-30 19:45:07 +0200

    Fix queryClient.invalidateQueries call in useUpdateUser.js

--- the-wild-oasis/src/features/authentication/useUpdateUser.js
+++ the-wild-oasis/src/features/authentication/useUpdateUser.js
@@ -10,7 +10,7 @@ export function useUpdateUser() {
     onSuccess: ({ user }) => {
       toast.success("User account successfully updated")
       queryClient.setQueryData(["user"], user)
-      queryClient.invalidateQueries({ queryKey: ["user"] })
+      //queryClient.invalidateQueries({ queryKey: ["user"] })
     },
     onError: (error) => toast.error(error.message),
   })
