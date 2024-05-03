a6e8aae530898d176cd1f1c03d7333d8ac233d46
Christof Kälin <christof.kaelin@protonmail.com>
2024-05-03 13:13:17 +0200

    outsourced NavLink component for client-mode

--- nextjs-training-app-router/components/main-header/main-header.js
+++ nextjs-training-app-router/components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logoImg from "/public/images/logo.png"
 import classes from "./main-header.module.css"
 import Image from "next/image"
 import MainHeaderBackground from "./main-header-background"
+import NavLink from "./nav-link"
 
 export default function MainHeader() {
   return (
@@ -16,13 +17,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/">Home</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/meals">Browse Meals</Link>
-            </li>
-            <li>
-              <Link href="/community">Community</Link>
+              <NavLink href="/community">Community</NavLink>
             </li>
           </ul>
         </nav>
