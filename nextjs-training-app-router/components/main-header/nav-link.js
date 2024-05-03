79f8795e5d5b9e2b014b7ba3e5965b145443e596
Christof Kälin <christof.kaelin@protonmail.com>
2024-05-03 13:16:39 +0200

    moved css to NavLink

--- nextjs-training-app-router/components/main-header/nav-link.js
+++ nextjs-training-app-router/components/main-header/nav-link.js
@@ -8,7 +8,11 @@ export default function NavLink({ href, children }) {
   return (
     <Link
       href={href}
-      className={path.startsWith(href) ? classes.active : undefined}>
+      className={
+        path.startsWith(href)
+          ? `${classes.link} ${classes.active}`
+          : classes.link
+      }>
       {children}
     </Link>
   )
