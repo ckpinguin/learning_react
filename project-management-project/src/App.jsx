b6e37a76161870cd3887dc6141a073ce81230e39
Christof Kälin <christof.kaelin@protonmail.com>
2024-05-07 16:08:56 +0200

    updates

--- project-management-project/src/App.jsx
+++ project-management-project/src/App.jsx
@@ -1,12 +1,52 @@
+import { useState } from "react"
 import NewProject from "./components/NewProject"
 import NoProjectSelected from "./components/NoProjectSelected"
 import ProjectsSidebar from "./components/ProjectsSidebar"
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined, // undefined = no current action
+    projects: [],
+  })
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      }
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      }
+    })
+  }
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null, // null = starting new project action,
+      }
+    })
+  }
+
+  let content
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
+      {content}
     </main>
   )
 }
