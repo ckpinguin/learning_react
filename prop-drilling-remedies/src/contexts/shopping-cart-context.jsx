3591004f1c2785e2ce1e2be8996a6c6d7536d99d
Christof Kälin <christof.kaelin@protonmail.com>
2024-05-24 17:16:46 +0200

    made all component that need it consume the context

--- prop-drilling-remedies/src/contexts/shopping-cart-context.jsx
+++ prop-drilling-remedies/src/contexts/shopping-cart-context.jsx
@@ -3,4 +3,5 @@ import { createContext } from "react"
 export const CartContext = createContext({
   items: [],
   addItemToCart: (item) => {}, // dummy function for autocompletion
+  updateItemQuantity: (productId, amount) => {}, // dummy function for autocompletion
 })
