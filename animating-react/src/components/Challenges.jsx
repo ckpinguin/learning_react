67e699d51f96f51b6eb6713b471179fe68b96798
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-05 12:45:58 +0200

    corrected for last element and multiple elements in AnimatePresence

--- animating-react/src/components/Challenges.jsx
+++ animating-react/src/components/Challenges.jsx
@@ -40,9 +40,10 @@ export default function Challenges() {
         challenges={filteredChallenges}
         onSelectType={handleSelectType}
         selectedType={selectedType}>
-        <AnimatePresence>
+        <AnimatePresence mode="wait">
           {displayedChallenges.length > 0 && (
             <motion.ol
+              key="list"
               exit={{ y: -30, opacity: 0 }}
               className="challenge-items">
               <AnimatePresence>
@@ -57,8 +58,16 @@ export default function Challenges() {
               </AnimatePresence>
             </motion.ol>
           )}
+          {displayedChallenges.length === 0 && (
+            <motion.p
+              key="fallback"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}>
+              No challenges found.
+            </motion.p>
+          )}
         </AnimatePresence>
-        {displayedChallenges.length === 0 && <p>No challenges found.</p>}
       </ChallengeTabs>
     </div>
   )
