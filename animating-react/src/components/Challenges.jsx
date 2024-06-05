d2f371032b8d4b2cb41e1425bc85ad070886b66b
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-05 12:38:56 +0200

    last list element handled with AnimatePresence

--- animating-react/src/components/Challenges.jsx
+++ animating-react/src/components/Challenges.jsx
@@ -3,7 +3,7 @@ import { useContext, useState } from "react"
 import { ChallengesContext } from "../store/challenges-context.jsx"
 import ChallengeItem from "./ChallengeItem.jsx"
 import ChallengeTabs from "./ChallengeTabs.jsx"
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 
 export default function Challenges() {
   const { challenges } = useContext(ChallengesContext)
@@ -40,20 +40,24 @@ export default function Challenges() {
         challenges={filteredChallenges}
         onSelectType={handleSelectType}
         selectedType={selectedType}>
-        {displayedChallenges.length > 0 && (
-          <ol className="challenge-items">
-            <AnimatePresence>
-              {displayedChallenges.map((challenge) => (
-                <ChallengeItem
-                  key={challenge.id}
-                  challenge={challenge}
-                  onViewDetails={() => handleViewDetails(challenge.id)}
-                  isExpanded={expanded === challenge.id}
-                />
-              ))}
-            </AnimatePresence>
-          </ol>
-        )}
+        <AnimatePresence>
+          {displayedChallenges.length > 0 && (
+            <motion.ol
+              exit={{ y: -30, opacity: 0 }}
+              className="challenge-items">
+              <AnimatePresence>
+                {displayedChallenges.map((challenge) => (
+                  <ChallengeItem
+                    key={challenge.id}
+                    challenge={challenge}
+                    onViewDetails={() => handleViewDetails(challenge.id)}
+                    isExpanded={expanded === challenge.id}
+                  />
+                ))}
+              </AnimatePresence>
+            </motion.ol>
+          )}
+        </AnimatePresence>
         {displayedChallenges.length === 0 && <p>No challenges found.</p>}
       </ChallengeTabs>
     </div>
