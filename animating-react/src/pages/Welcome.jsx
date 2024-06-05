9418086b5db35a82d7f074efd0ab566ea4157156
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-05 15:08:39 +0200

    fixed scrolling animation

--- animating-react/src/pages/Welcome.jsx
+++ animating-react/src/pages/Welcome.jsx
@@ -15,7 +15,7 @@ export default function WelcomePage() {
   )
   const yHero = useTransform(scrollY, [0, 200], [0, -150])
   const opacityHero = useTransform(scrollY, [0, 300, 500], [1, 1, 0])
-  const scaleText = useTransform(scrollY, [0, 300], 1, 1.5)
+  const scaleText = useTransform(scrollY, [0, 300], [1, 1.5])
   const yText = useTransform(scrollY, [0, 200, 300, 500], [0, 50, 50, 300])
 
   return (
