8bd9c37be33f664cb37d85a93da7273806057198
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-05 15:26:50 +0200

    compound component - start

--- react-patterns-and-best-practices/src/App.jsx
+++ react-patterns-and-best-practices/src/App.jsx
@@ -1,5 +1,34 @@
+import Accordion from "./components/Accordion"
+import AccordionItem from "./components/AccordionItem"
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion>
+          <AccordionItem title="We deliver on time">
+            <article>
+              <p>
+                We deliver on time. We are a team of dedicated professionals who
+                are committed to delivering your project on time.
+              </p>
+              <p>We pay on delay.</p>
+            </article>
+          </AccordionItem>
+          <AccordionItem title="We are experienced">
+            <article>
+              <p>
+                We deliver on time. We are a team of dedicated professionals who
+                are committed to delivering your project on time.
+              </p>
+              <p>We are in the business for more than 20 years.</p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
